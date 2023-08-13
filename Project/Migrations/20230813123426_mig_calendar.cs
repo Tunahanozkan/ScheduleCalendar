e8d7847336fbe8922b98df332359e2a54aea7ec3
tunahan özkan <mhnds_tunahan@hotmail.com>
2023-08-13 16:00:50 +0300

    Add project files.

---
+++ Project/Migrations/20230813123426_mig_calendar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_calendar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Calendars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Calendars", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Calendars");
+        }
+    }
+}
---
+++ Project/Models/CalendarContext.cs
@@ -0,0 +1,24 @@
+﻿    using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Project.Models
+{
+    public class CalendarContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-GQ26LQR;database= CalendarSchedule;integrated security = true;TrustServerCertificate=True");
+        }
+        public DbSet<Calendar> Calendars { get; set; }
+    }
+
+    public class Calendar
+    {
+        public int Id { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+        public string? Color { get; set; }
+    }
+
+}
